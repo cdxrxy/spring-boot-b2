0c00e4cf68a320fc6296ec3d3f4de997c1bfeabc
cdxrxy <marzoeff@gmail.com>
2023-05-15 21:29:12 +0600

    n + 1 fix for orders -> users

--- src/main/java/com/example/springbootb2/repository/OrderRepo.java
+++ src/main/java/com/example/springbootb2/repository/OrderRepo.java
@@ -1,7 +1,12 @@
 package com.example.springbootb2.repository;
 
 import com.example.springbootb2.model.Order;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepo extends JpaRepository<Order, Long> {
+    @EntityGraph(attributePaths = "user")
+    List<Order> findAll();
 }
